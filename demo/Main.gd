e577dc9fa7ac04a3904012720c68688348d1aa40
“linuxtechlab” <mounirtohami@gmail.com>
2021-02-04 02:18:06 +0200

    fixed some issues / MounirTohami

--- demo/Main.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _ready():
-	for node in $NodePathTracker.get_nodes():
-		if node is Button:
-			node.connect("pressed", self, "_on_button_pressed", [node])
-
-func _on_button_pressed(button : Button):
-	$NodePathTracker.get_node_by_name("PressedLabel").text = "Button " + button.name + " pressed!"
